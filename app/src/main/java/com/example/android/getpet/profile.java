0473a2b1810b193eefde62707f7dc551f9136ee3
Arsh Singh <arshsingh@Arshs-MacBook-Air.local>
2022-11-02 20:40:15 +0530

    profile image update

--- app/src/main/java/com/example/android/getpet/profile.java
+++ app/src/main/java/com/example/android/getpet/profile.java
@@ -15,10 +15,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.UploadTask;
@@ -164,14 +168,14 @@ public class profile extends AppCompatActivity {
         });
     }
 
-    private void getDataForProfilePic() {
-            FirebaseDatabase.getInstance().getReference("users/"+FirebaseAuth.getInstance().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+    private void getDataForProfilePic(){
+        FirebaseDatabase.getInstance().getReference("users/"+FirebaseAuth.getInstance().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                 mUrl = snapshot.getValue(User.class).getProfilePic();
+                mUrl = snapshot.getValue(User.class).getProfilePic();
                 Glide.with(getApplicationContext()).load(mUrl).error(R.drawable.account_img)
-                .placeholder(R.drawable.account_img)
-                .into(imgProfile);
+                        .placeholder(R.drawable.account_img)
+                        .into(imgProfile);
             }
 
             @Override
