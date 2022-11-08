f7cc1b3239fd44c0d5062b210f1db383e1c3da68
himcoder0 <100153753+himcoder0@users.noreply.github.com>
2022-11-09 01:55:50 +0530

    Update Token.java

--- app/src/main/java/com/example/android/getpet/SendNotification/Token.java
+++ app/src/main/java/com/example/android/getpet/SendNotification/Token.java
@@ -1,4 +1,20 @@
 package com.example.android.getpet.SendNotification;
 
 public class Token {
+    private String token;
+
+    public Token(String token) {
+        this.token = token;
+    }
+
+    public Token(){
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
