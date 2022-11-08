128dd09e8e5ca5acaa0d13662f13a80d87c36c86
himcoder0 <100153753+himcoder0@users.noreply.github.com>
2022-11-09 01:49:32 +0530

    Update Data.java

--- app/src/main/java/com/example/android/getpet/SendNotification/Data.java
+++ app/src/main/java/com/example/android/getpet/SendNotification/Data.java
@@ -1,4 +1,30 @@
 package com.example.android.getpet.SendNotification;
 
 public class Data {
+    private String Title;
+    private String Message;
+
+    public Data(String title, String message) {
+        Title = title;
+        Message = message;
+    }
+
+    public Data(){
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
+    public void setMessage(String message) {
+        Message = message;
+    }
 }
