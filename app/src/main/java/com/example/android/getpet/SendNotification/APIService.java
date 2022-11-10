4fcd11aea757a41254ad2246e9c4d6a002756252
himcoder0 <100153753+himcoder0@users.noreply.github.com>
2022-11-10 22:15:42 +0530

    Update APIService.java

--- app/src/main/java/com/example/android/getpet/SendNotification/APIService.java
+++ app/src/main/java/com/example/android/getpet/SendNotification/APIService.java
@@ -9,10 +9,14 @@ public interface APIService {
     @Headers(
             {
                     "Content-Type:application/json",
+
+                    // server key for using firebase messaging services
                     "Authorization:key=AAAAMIzI_cU:APA91bHVvU61_sLjzaLWsvu1RJpKhRzGHGizHKzFPCTILKZDSekDCA3zMLCUGRs40Zm-WyndVwklblrtBXjIyT-s-AcyqHJE49Mg-tIzg_y3R3cR_OhUrTXYCwn5JW5nJUnaTbusu3W-" // Your server key refer to video for finding your server key
             }
     )
 
     @POST("fcm/send")
+
+    //
     Call<MyResponse> sendNotifcation(@Body NotificationSender body);
 }
