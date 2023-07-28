c7b6f99d235956a9295ab432866e739b8805148a
himcoder0 <100153753+himcoder0@users.noreply.github.com>
2023-07-28 10:55:51 +0530

    Create Owner.java

---
+++ app/src/main/java/com/example/android/getpet/models/Owner.java
@@ -0,0 +1,21 @@
+package com.example.android.getpet.models;
+
+public interface Owner {
+
+    public String getOwnerKey();
+
+    public void setOwnerKey(String ownerkey) ;
+
+    public String getOwnerName() ;
+
+    public void setOwnerName(String ownerName) ;
+
+    public String getOwnerEmail() ;
+
+    public void setOwnerEmail(String ownerEmail) ;
+
+    public String getOwnerProfilePic() ;
+
+    public void setOwnerProfilePic(String ownerProfilePic) ;
+
+}
