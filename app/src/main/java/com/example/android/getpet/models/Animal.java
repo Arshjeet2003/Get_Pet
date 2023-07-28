a3ad2248666ba163c788f3b7f8665e351c105ae5
himcoder0 <100153753+himcoder0@users.noreply.github.com>
2023-07-28 10:55:11 +0530

    Create Animal.java

---
+++ app/src/main/java/com/example/android/getpet/models/Animal.java
@@ -0,0 +1,57 @@
+package com.example.android.getpet.models;
+
+public interface Animal {
+
+    public String getDescription();
+
+    public void setDescription(String description) ;
+
+    public String getLocation() ;
+
+    public void setLocation(String location);
+
+    public String getAnimalName();
+
+    public void setAnimalName(String animalName) ;
+
+    public String getPetKey();
+
+    public void setPetKey(String petKey);
+
+    public String getAnimal() ;
+
+    public void setAnimal(String animal) ;
+
+    public String getKey() ;
+
+    public void setKey(String key) ;
+
+    public String getProfilePic() ;
+
+    public void setProfilePic(String profilePic) ;
+
+    public String getBreed() ;
+
+    public void setBreed(String breed) ;
+
+    public String getAge() ;
+
+    public void setAge(String age) ;
+
+    public String getSize() ;
+
+    public void setSize(String size) ;
+
+    public String getGender() ;
+
+    public void setGender(String gender);
+
+    public String getPetLat() ;
+
+    public void setPetLat(String petLat);
+
+    public String getPetLong() ;
+
+    public void setPetLong(String petLong);
+
+}
